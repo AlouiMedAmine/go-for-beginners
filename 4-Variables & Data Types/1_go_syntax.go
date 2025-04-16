29d1695a374b7a6296dc470bc451516ea700d2e9
AlouiMedAmine <mohammed.Aminaloui@gmail.com>
2025-04-17 00:37:47 +0200

    first section

---
+++ 4-Variables & Data Types/1_go_syntax.go
@@ -0,0 +1,40 @@
+//          ****** Go Syntax ******
+
+// *****1- Go Syntax
+/*
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Hello World!")
+	fmt.Println("Hello World!")
+
+	fmt.Println("Hello World!")
+	fmt.Println("Hello World!")
+	fmt.Println("Hello World!")
+
+}
+*/
+// ****** 2-Go Statements
+
+/*
+package main
+import ("fmt")
+
+func main()
+{
+  fmt.Println("Hello World!")
+}
+*/
+
+// ******* Go Compact Code
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() { fmt.Println("Hello World!") }
